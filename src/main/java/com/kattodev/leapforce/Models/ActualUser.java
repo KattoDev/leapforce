489f4d73ac16d082f2a2b7f8d9d95d608459139e
KattoDev <alexisrincon27@outlook.com>
2024-12-11 12:16:44 -0500

    add ActualUser Singleton

---
+++ src/main/java/com/kattodev/leapforce/Models/ActualUser.java
@@ -0,0 +1,45 @@
+package com.kattodev.leapforce.Models;
+
+/**
+ * Singleton class to get the Actual User within the app
+ */
+public class ActualUser {
+    private static ActualUser instance;
+    private User user;
+
+
+    /**
+     * Static method to get the actual instance of {@link ActualUser}.
+     * If the instance is null, this method creates a new instance with a empty {@link User}
+     * @return the instance of the {@link ActualUser}
+     */
+    public static ActualUser getInstance() {
+        if (instance == null) {
+            instance = new ActualUser();
+        }
+        return instance;
+    }
+
+    /**
+     * Destroys the {@link ActualUser} instance
+     */
+    public void destroyInstance() {
+        instance = null;
+    }
+
+    /**
+     * Sets a user to be globally used with the app
+     * @param user an instance of {@link User}
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /**
+     * Gets the user within the global instance
+     * @return a global instance of {@link ActualUser} {@link User}
+     */
+    public User getUser() {
+        return user;
+    }
+}
