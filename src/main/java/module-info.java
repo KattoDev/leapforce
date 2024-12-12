049404d7d5551e20695d7119bc735210e878ddf0
KattoDev <alexisrincon27@outlook.com>
2024-12-12 13:34:49 -0500

    Add required dependencies and packages

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,16 @@ module com.kattodev.leapforce {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires com.google.gson;
+
     requires org.kordamp.bootstrapfx.core;
+
     requires java.sql;
 
+    opens com.kattodev.leapforce.Apps to javafx.fxml;
+    exports com.kattodev.leapforce.Apps;
+    opens com.kattodev.leapforce.Controllers to javafx.fxml;
+    exports com.kattodev.leapforce.Controllers;
+    exports com.kattodev.leapforce.APIClient;
+    opens com.kattodev.leapforce.APIClient to javafx.fxml;
 }
