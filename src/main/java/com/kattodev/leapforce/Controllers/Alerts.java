b5d4c4fdd37d528cda4a08b2a114c8d4cba6ff3b
KattoDev <alexisrincon27@outlook.com>
2024-12-12 13:38:08 -0500

    Add easy alerts handler system

---
+++ src/main/java/com/kattodev/leapforce/Controllers/Alerts.java
@@ -0,0 +1,20 @@
+package com.kattodev.leapforce.Controllers;
+
+import javafx.scene.control.Alert;
+
+public class Alerts {
+
+    /**
+     * Creates an alert that shows and wait user interaction
+     * @param alertType the alert type
+     * @param title the alert title
+     * @param body the alert body
+     */
+    public static void showAlert(Alert.AlertType alertType, String title, String body) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(body);
+        alert.showAndWait();
+    }
+}
