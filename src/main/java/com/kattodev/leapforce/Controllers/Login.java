8abb08b388c8ddbb5a8d781ce093a108dcef9de9
KattoDev <alexisrincon27@outlook.com>
2024-12-13 00:52:53 -0500

    Login controller with basic Auth

--- src/main/java/com/kattodev/leapforce/Controllers/Login.java
+++ src/main/java/com/kattodev/leapforce/Controllers/Login.java
@@ -2,13 +2,14 @@ package com.kattodev.leapforce.Controllers;
 
 import DebugHandler.Debug;
 
-import com.kattodev.leapforce.APIClient.DatabaseConfig;
 import com.kattodev.leapforce.APIClient.DatabaseConnection;
 
 import com.kattodev.leapforce.Models.ActualUser;
 import com.kattodev.leapforce.Models.SystemMessages;
 import com.kattodev.leapforce.Models.User;
+
 import javafx.fxml.FXML;
+
 import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
 
