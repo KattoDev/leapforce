d48816c4d28cff224e2efa4220bb916e3f03fea3
KattoDev <alexisrincon27@outlook.com>
2024-12-13 17:33:45 -0500

    Add WIP Scene switch controller

--- src/main/java/com/kattodev/leapforce/Controllers/Login.java
+++ src/main/java/com/kattodev/leapforce/Controllers/Login.java
@@ -4,14 +4,19 @@ import DebugHandler.Debug;
 
 import com.kattodev.leapforce.APIClient.DatabaseConnection;
 
-import com.kattodev.leapforce.Models.ActualUser;
-import com.kattodev.leapforce.Models.SystemMessages;
+import com.kattodev.leapforce.Apps.SideBar;
+import com.kattodev.leapforce.Utils.ActualUser;
+import com.kattodev.leapforce.Utils.SystemMessages;
 import com.kattodev.leapforce.Models.User;
 
+import com.kattodev.leapforce.Utils.Alerts;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Login {
     @FXML
@@ -20,8 +25,8 @@ public class Login {
     @FXML
     private com.gluonhq.charm.glisten.control.TextField txt_password;
 
-    public void btn_login_click(MouseEvent mouseEvent) {
-        new Debug("Click");
+    public void btn_login_click(MouseEvent mouseEvent) throws IOException {
+        new Debug("Login button Click");
 
         String email = txt_email.getText();
         String password = txt_password.getText();
@@ -29,9 +34,21 @@ public class Login {
         User tempUser = new User(email, password);
         if (!(tempUser.Auth(DatabaseConnection.getInstance().connection))) {
             Alerts.showAlert(Alert.AlertType.WARNING, SystemMessages.LoginErrorTitle, SystemMessages.LoginErrorBody);
+        } else {
+
+            Stage stage = (Stage) ((javafx.scene.Node) mouseEvent.getSource()).getScene().getWindow();
+            stage.close();
+
+            // Lanzar el Dashboard
+            try {
+                SideBar sideBar = new SideBar();
+                Stage sideBarStage = new Stage();
+                sideBar.start(sideBarStage);
+            } catch (NullPointerException nullPointerException) {
+                nullPointerException.getCause();
+            }
+
+            new Debug(ActualUser.getInstance().getUser().toString());
         }
-
-        new Debug(ActualUser.getInstance().getUser().toString());
-
     }
 }
