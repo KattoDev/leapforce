02b842aabea54ff11d73b742050972e5a4da984d
KattoDev <alexisrincon27@outlook.com>
2024-12-18 15:23:56 -0500

    Add getter to Departments Model
    GetDepartmentName() and GetDepartmentDID()

--- src/main/java/com/kattodev/leapforce/Models/Department.java
+++ src/main/java/com/kattodev/leapforce/Models/Department.java
@@ -1,5 +1,11 @@
 package com.kattodev.leapforce.Models;
 
+import DebugHandler.Debug;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Department {
@@ -40,5 +46,45 @@ public class Department {
 
     public void setProjects(ArrayList<Integer> projects) {
         this.projects = projects;
+    }
+
+    public String getDepartmentName(long DID, Connection databaseConnection) {
+        String query = "SELECT name FROM departments WHERE DID = ?";
+        String error = "ERROR EN LA BUSQUEDA DEL DEPARTAMENTO";
+
+        try (PreparedStatement pstmt = databaseConnection.prepareStatement(query)) {
+            pstmt.setLong(1, DID);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (!rs.next()) {
+                    return error;
+                }
+
+                return rs.getString("name");
+            }
+        } catch (SQLException sqlException) {
+            new Debug(error + "\n" + sqlException.getMessage());
+            return error;
+        }
+    }
+
+    public String getDepartmentDID(String departmentName, Connection databaseConnection) {
+        String query = "SELECT DID FROM departments WHERE name = ?";
+        String error = "ERROR EN LA BUSQUEDA DEL DEPARTAMENTO";
+
+        try (PreparedStatement pstmt = databaseConnection.prepareStatement(query)) {
+            pstmt.setString(1, departmentName);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (!rs.next()) {
+                    return error;
+                }
+
+                return rs.getString("DID");
+            }
+        } catch (SQLException sqlException) {
+            new Debug(error + "\n" + sqlException.getMessage());
+            return error;
+        }
     }
 }
