5507a9bf4b85fd3ade689038b085f2699eaf8543
KattoDev <alexisrincon27@outlook.com>
2024-12-18 15:21:33 -0500

    Add Main launcher

---
+++ src/main/java/com/kattodev/leapforce/Main.java
@@ -0,0 +1,9 @@
+package com.kattodev.leapforce;
+
+import com.kattodev.leapforce.Apps.Login;
+
+public class Main {
+    public static void main(String[] args) {
+        Login.main(args);
+    }
+}
