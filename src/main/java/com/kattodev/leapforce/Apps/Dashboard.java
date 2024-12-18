1e2c8dd2b879df75f9c5ab9de08d3fbc916fd7cf
KattoDev <alexisrincon27@outlook.com>
2024-12-18 15:27:12 -0500

    Add dashboard main app

---
+++ src/main/java/com/kattodev/leapforce/Apps/Dashboard.java
@@ -0,0 +1,36 @@
+package com.kattodev.leapforce.Apps;
+
+import DebugHandler.Debug;
+import com.kattodev.leapforce.Utils.ActualUser;
+import com.kattodev.leapforce.Utils.WindowSize;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Dashboard extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                Login.class.getResource("/com/kattodev/leapforce/Views/Dashboard.fxml"));
+        Scene scene = new Scene(
+                fxmlLoader.load(),
+                WindowSize.getDashboard()[0],
+                WindowSize.getDashboard()[1]);
+
+        stage.setTitle("LEAPFORCE - DASHBOARD");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+
+        new Debug(ActualUser.getInstance().getUser().toString());
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
